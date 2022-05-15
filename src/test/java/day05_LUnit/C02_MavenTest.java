405db8c725b57b1bd835007c6efcbd65685ed593
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-05-15 21:39:06 +0300

    15.05.2022

--- src/test/java/day05_LUnit/C02_MavenTest.java
+++ src/test/java/day05_LUnit/C02_MavenTest.java
@@ -1,7 +1,9 @@
 package day05_LUnit;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -16,15 +18,31 @@ public class C02_MavenTest {
         //1. http://zero.webappsecurity.com sayfasina gidin
         driver.get("http://zero.webappsecurity.com");
         //2. Signin buttonuna tiklayin
-
+        driver.findElement(By.xpath("//button[@id='signin_button']")).click();
         //3. Login alanine “username” yazdirin
+        driver.findElement(By.xpath("//input[@id='user_login']")).sendKeys("username");
         //4. Password alanine “password” yazdirin
-        //5. Sign in buttonuna tiklayin
-        //6. Pay Bills sayfasina gidin
+        driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("password");
+        //5. Sign in buttonuna tiklayin(hata msjı ıcın back tusuna tıklayın)
+        driver.findElement(By.xpath("//input[@name='submit']")).click();
+        driver.navigate().back();
+        //6. online bancking menusunden Pay Bills sayfasina gidin
+        driver.findElement(By.xpath("(//*[text()='Online Banking'])[1]")).click();
+        driver.findElement(By.xpath("//span[@id='pay_bills_link']")).click();
         //7. amount kismina yatirmak istediginiz herhangi bir miktari yazin
+        driver.findElement(By.xpath("//input[@id='sp_amount']")).sendKeys("500");
         //8. tarih kismina “2020-09-10” yazdirin
+        driver.findElement(By.xpath("//input[@id='sp_date']")).sendKeys("2020-09-10");
         //9. Pay buttonuna tiklayin
-        //10. “The payment was successfully submitted.” mesajinin ciktigini control edin
+        driver.findElement(By.id("pay_saved_payees")).click();
+        //10. “The payment was successfully submitted.” mesajinin ciktigini kontrol edin
+        WebElement sonucYazisiElementi=driver.findElement(By.xpath("//div[@id='alert_content']"));
+        if(sonucYazisiElementi.isDisplayed()){
+            System.out.println("Test PASSED");
+        }else {
+            System.out.println("Test FAILED");
+        }
+        driver.close();
 
     }
 }
