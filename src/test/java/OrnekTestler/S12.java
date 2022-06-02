e39ae5a6c2eed6b87670e59b568208f412095d6e
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-06-02 12:21:41 +0300

    02.06.2022

--- src/test/java/OrnekTestler/S12.java
+++ src/test/java/OrnekTestler/S12.java
@@ -1,6 +1,12 @@
 package OrnekTestler;
 
-public class S12 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class S12 extends TestBase {
     /*
     Asagidaki testi text’leri kullanarak locate edin
 1- https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
@@ -9,4 +15,25 @@ public class S12 {
 4- Delete tusuna basin
 5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
      */
+
+    @Test
+    public void test01() {
+        //1- https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+
+        //2- Add Element butonuna basin
+        driver.findElement(By.xpath("//button[@onclick='addElement()']")).click();
+
+        //3- Delete butonu’nun gorunur oldugunu test edin
+       WebElement deleteButton= driver.findElement(By.xpath("//button[@onclick='deleteElement()']"));
+       Assert.assertTrue(deleteButton.isDisplayed());
+
+        //4- Delete tusuna basin
+        deleteButton.click();
+
+        //5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
+        WebElement addRemoveElements = driver.findElement(By.xpath("//h3"));
+        Assert.assertTrue(addRemoveElements.isDisplayed());
+
+    }
 }
--- src/test/java/OrnekTestler/S13.java
+++ src/test/java/OrnekTestler/S13.java
@@ -1,6 +1,10 @@
 package OrnekTestler;
 
-public class S13 {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class S13 extends TestBase {
     /*
     1 ) Bir class oluşturun : Locators_css
 2 ) Main method oluşturun ve aşağıdaki görevi tamamlayın.
@@ -13,4 +17,21 @@ i. Username : testtechproed@gmail.com
 ii. Password : Test1234!
 NOT: cssSelector kullanarak elementleri locate ediniz
      */
+
+    @Test
+    public void test01() {
+        //a. Verilen web sayfasına gidin. http://a.testaddressbook.com/sign_in
+        driver.get("http://a.testaddressbook.com/sign_in");
+
+        //b. Locate email textbox
+        driver.findElement(By.cssSelector("input[type='email']")).sendKeys("testtechproed@gmail.com");
+        driver.findElement(By.cssSelector("input[id='session_password']")).sendKeys("Test1234!");
+
+        //c. Locate password textbox ve
+        //d. Locate signin button
+        //e. Asagidaki kullanıcı adını ve şifreyi girin ve sign in düğmesini tıklayın
+        //i. Username : testtechproed@gmail.com
+        //ii. Password : Test1234!
+        //NOT: cssSelector kullanarak elementleri locate ediniz
+    }
 }
